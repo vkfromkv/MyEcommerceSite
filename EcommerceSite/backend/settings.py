115c4994f05a8762f1ab0d47d29e76157965e53c
Varun Kumar Valaboju <valav01@pfw.edu>
2023-12-05 04:12:24 -0500

    allowed hosts change

--- EcommerceSite/backend/settings.py
+++ EcommerceSite/backend/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-# ALLOWED_HOSTS =os.environ.get("ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS =os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 
